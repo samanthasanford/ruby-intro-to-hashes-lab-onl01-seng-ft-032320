ff6214c718b23424d3b8cb37c8456cc4a766de37
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-28 01:56:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -63,7 +63,7 @@ def monopoly_with_fourth_tier
           "mortgage_value" => "$400"
         },
         :shortline => {
-          "mortgage_value" => "800"
+          "mortgage_value" => "$800"
           
         }
       },
