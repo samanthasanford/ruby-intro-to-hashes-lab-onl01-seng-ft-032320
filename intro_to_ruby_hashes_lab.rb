71210baf84ca210a83a50a07ad1c8518a3325c6c
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-28 01:51:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :names => {
-        :reading_railroads => { },
+        :reading_railroad => { },
         :pennsylvania_railroad => { },
         :b_and_o_railroad => { },
         :shortline => { }
